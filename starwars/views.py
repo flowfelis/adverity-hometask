e62480ed403b2c4685a5e6c1b154d983247145bf
Ali <alicandonmez90@gmail.com>
2021-10-13 21:12:59 +0200

    update fetching from api and use petl for csv operations

--- starwars/views.py
+++ starwars/views.py
@@ -1,3 +1,7 @@
+import datetime
+import uuid
+
+import petl as etl
 import requests
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -28,12 +32,36 @@ class CollectionDetailView(DetailView):
 
 class FetchCollection(View):
     http_method_names = ['get']
+
     URL = 'https://swapi.dev/api/people'
     ITEM_PER_PAGE = 10
+    DOWNLOAD_DIRECTORY = 'files'
 
     def get(self, request, *args, **kwargs):
-        self.fetch_collection()
+        self.fetch_characters_from_api()
+        self.transform_and_write_to_csv()
         return HttpResponseRedirect(reverse('collection-list'))
+
+    def fetch_characters_from_api(self):
+        self.all_characters = []
+        for page_number in range(1, self.total_page_number() + 1):
+            # Fetch raw data
+            resp = requests.get(f'{FetchCollection.URL}/?page={page_number}')
+            data = resp.json()
+            some_characters = data.get('results')
+            self.all_characters.extend(some_characters)
+
+    def transform_and_write_to_csv(self):
+        filename = f'{FetchCollection.DOWNLOAD_DIRECTORY}/{str(uuid.uuid4())}.csv'
+
+        header = [list(self.all_characters[0].keys())]
+        values = [list(character.values()) for character in self.all_characters]
+        table = header + values
+
+        etl.addfield(table, 'date', lambda row: self.convert_to_date(row['edited'])) \
+            .convert('homeworld', lambda value: self.resolve_homeworld(value)) \
+            .cutout('films', 'species', 'vehicles', 'starships', 'created', 'edited', 'url') \
+            .tocsv(filename)
 
     def total_page_number(self):
         resp = requests.get(FetchCollection.URL)
@@ -43,31 +71,11 @@ class FetchCollection(View):
             else total_item // FetchCollection.ITEM_PER_PAGE + 1
         return total_page_number
 
-    def fetch_collection(self):
-        # there is one extra request being made for only fetching "count" number.
-        # but this is preferred for code-readability over performance(because not significant)
+    def convert_to_date(self, raw_date):
+        d = datetime.datetime.strptime(raw_date[:raw_date.find('T')], '%Y-%m-%d')
+        return str(d.date())
 
-        # url = 'https://swapi.dev/api/people'
-        # get "count" (total_item)
-        # item_per_page = 10
-        # total_page_number = count // item_per_page if count % item_per_page == 0 else count // item_per_page + 1
-        # for page_number in range(1, total_page_number+1):
-        #   self.data = fetch from url + '/?page=page_number' (Using threading)
-        #   self.save()
-        pass
-
-    def save(self):
-        # transformed_data = self.transform_data(self.data)
-        # self.save_to_csv(transformed_data)
-        # metadata = fetch metadata
-        # self.save_to_db(metadata)
-        pass
-
-    def transform_data(self, data):
-        pass
-
-    def save_to_csv(self, data):
-        pass
-
-    def save_to_db(self, data):
-        pass
+    def resolve_homeworld(self, planet_url):
+        resp = requests.get(planet_url)
+        data = resp.json()
+        return data['name']
