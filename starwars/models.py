d204d9bca49beb7d3f754bd4b6df7827582e104a
Ali <alicandonmez90@gmail.com>
2021-10-13 22:22:50 +0200

    order by date_added desc

--- starwars/models.py
+++ starwars/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Collection(models.Model):
     filename = models.CharField(max_length=256)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_added']
