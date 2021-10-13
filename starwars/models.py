93d0b959dff97d54d90bbd4eb976aa5088c5d900
Ali <alicandonmez90@gmail.com>
2021-10-13 16:50:34 +0200

    create Collection model

--- starwars/models.py
+++ starwars/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Collection(models.Model):
+    filename = models.CharField(max_length=256)
+    date_added = models.DateTimeField(auto_now_add=True)
