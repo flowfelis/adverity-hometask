3008bef0c0072ae5fc53e0c1bf728b7d2ae20bd8
Ali <alicandonmez90@gmail.com>
2021-10-13 23:26:15 +0200

    display detail view from csv

--- starwars/views.py
+++ starwars/views.py
@@ -12,6 +12,10 @@ from django.views.generic import ListView
 
 from starwars.models import Collection
 
+DOWNLOAD_DIRECTORY = 'files'
+URL = 'https://swapi.dev/api/people'
+ITEM_PER_PAGE = 10
+
 
 class CollectionListView(ListView):
     http_method_names = ['get']
@@ -22,31 +26,26 @@ class CollectionDetailView(DetailView):
     http_method_names = ['get']
     model = Collection
 
-    def render_to_response(self, context, **response_kwargs):
-        filename = context.get('collection').filename
-        # read from csv
-        # with open(filename) as f:
-        #     pass
-        # update context with csv content
-        return super().render_to_response(context, **response_kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        file_location = f'{DOWNLOAD_DIRECTORY}/{context.get("collection").filename}'
+        table = etl.fromcsv(file_location)
+        context['table'] = list(table)
+        return context
 
 
 class FetchCollection(View):
     http_method_names = ['get']
 
-    URL = 'https://swapi.dev/api/people'
-    ITEM_PER_PAGE = 10
-    DOWNLOAD_DIRECTORY = 'files'
-
     def write_metadata_to_db(self):
         Collection.objects.create(filename=self.filename)
 
     def page_numbers(self):
-        resp = requests.get(FetchCollection.URL)
+        resp = requests.get(URL)
         total_item = resp.json().get('count')
-        total_page_number = total_item // FetchCollection.ITEM_PER_PAGE \
-            if total_item % FetchCollection.ITEM_PER_PAGE == 0 \
-            else total_item // FetchCollection.ITEM_PER_PAGE + 1
+        total_page_number = total_item // ITEM_PER_PAGE \
+            if total_item % ITEM_PER_PAGE == 0 \
+            else total_item // ITEM_PER_PAGE + 1
         return range(1, total_page_number + 1)
 
     def convert_to_date(self, raw_date):
@@ -71,7 +70,7 @@ class FetchCollection(View):
         return HttpResponseRedirect(reverse('collection-list'))
 
     def fetch_from_api(self, page_number):
-        resp = requests.get(f'{FetchCollection.URL}/?page={page_number}')
+        resp = requests.get(f'{URL}/?page={page_number}')
         data = resp.json()
         return data.get('results')
 
@@ -85,7 +84,7 @@ class FetchCollection(View):
 
     def transform_and_write_to_csv(self):
         self.filename = f'{str(uuid.uuid4())}.csv'
-        save_to = f'{FetchCollection.DOWNLOAD_DIRECTORY}/{self.filename}'
+        file_location = f'{DOWNLOAD_DIRECTORY}/{self.filename}'
 
         header = [list(self.all_characters[0].keys())]
         values = [list(character.values()) for character in self.all_characters]
@@ -98,4 +97,4 @@ class FetchCollection(View):
             .addcolumn('homeworld', self.resolved_homeworld, index=8) \
             .addfield('date', lambda row: self.convert_to_date(row['edited'])) \
             .cutout('films', 'species', 'vehicles', 'starships', 'created', 'edited', 'url') \
-            .tocsv(save_to)
+            .tocsv(file_location)
