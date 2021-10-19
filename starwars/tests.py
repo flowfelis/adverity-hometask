a36051c93772e4af31dc09edcc1ccbf005373d8f
Ali <alicandonmez90@gmail.com>
2021-10-19 16:50:44 +0200

    write first test

--- starwars/tests.py
+++ starwars/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from starwars import views
+from starwars.models import Collection
 
-# Create your tests here.
+
+class FetchCollectionViewTests(TestCase):
+    def test_write_metadata_to_db(self):
+        instance = views.FetchCollectionView()
+        instance.filename = 'dummy-filename'
+        instance.write_metadata_to_db()
+        collection = Collection.objects.first()
+
+        self.assertIsNotNone(collection)
+        self.assertEqual('dummy-filename', collection.filename)
